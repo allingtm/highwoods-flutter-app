ab094c05733dd29b60369e0e6fe4e3cefbfbb2ad
Marc Allington <allingtm@gmail.com>
2026-01-19 10:53:55 +0000

    Clamped text size 0.85 to 1.3

--- lib/main.dart
+++ lib/main.dart
@@ -127,14 +127,22 @@ class _MainAppState extends ConsumerState<MainApp> {
     final router = ref.watch(goRouterProvider);
     final theme = ref.watch(themeDataProvider);
 
-    return MaterialApp.router(
-      title: 'Highwoods',
-      scaffoldMessengerKey: _scaffoldMessengerKey,
-      theme: theme,
-      themeAnimationDuration: const Duration(milliseconds: 300),
-      themeAnimationCurve: Curves.easeInOut,
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(
+        textScaler: MediaQuery.textScalerOf(context).clamp(
+          minScaleFactor: 0.85,
+          maxScaleFactor: 1.3,
+        ),
+      ),
+      child: MaterialApp.router(
+        title: 'Highwoods',
+        scaffoldMessengerKey: _scaffoldMessengerKey,
+        theme: theme,
+        themeAnimationDuration: const Duration(milliseconds: 300),
+        themeAnimationCurve: Curves.easeInOut,
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
